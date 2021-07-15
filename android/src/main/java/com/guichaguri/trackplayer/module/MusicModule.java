e4308ba47b39f8507e121f1616b0049c83acd854
Anjum Shrimali <ayshrimali@gmail.com>
2021-07-15 15:32:27 +0530

    added missing method

--- android/src/main/java/com/guichaguri/trackplayer/module/MusicModule.java
+++ android/src/main/java/com/guichaguri/trackplayer/module/MusicModule.java
@@ -337,7 +337,7 @@ public class MusicModule extends ReactContextBaseJavaModule implements ServiceCo
         waitForConnection(() -> {
             ExoPlayback playback = binder.getPlayback();
             if (playback instanceof NoOpPlayback) {
-
+                ((NoOpPlayback)playback).markConnecting(error);
             }
             callback.resolve(null);
         });
